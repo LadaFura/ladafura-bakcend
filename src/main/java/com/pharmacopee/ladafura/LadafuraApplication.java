9510011b0d5874bea233c8357d6445a99da6749d
ibecoul <boulby9023@gmail.com>
2026-09-24 11:49:31 +0000

    first commit

---
+++ src/main/java/com/pharmacopee/ladafura/LadafuraApplication.java
@@ -0,0 +1,13 @@
+package com.pharmacopee.ladafura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LadafuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LadafuraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pharmacopee/ladafura/LadafuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmacopee.ladafura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LadafuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
